28c9b4872cf39ce05bde64ead3f312745bb2acfb
Alexey Plotnik <odiszapc@gmail.com>
2014-11-18 02:30:29 +1000

    STEM-6

---
+++ components/clustermanager/src/main/java/org/stem/domain/DistributionManager.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2014 Alexey Plotnik
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.stem.domain;
+
+import org.stem.domain.topology.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class DistributionManager {
+
+    final Partitioner partitioner;
+    private Cluster cluster;
+    private ArrayBalancer keysDistributor;
+
+    public DistributionManager(Partitioner partitioner, Cluster cluster) {
+        this.partitioner = partitioner;
+        this.cluster = cluster;
+    }
+
+    public synchronized void computeDataMap() {
+        computeDataMap(cluster.descriptor().rf, cluster.descriptor().vBuckets, cluster.topology());
+    }
+
+    private void computeDataMap(int rf, int buckets, org.stem.domain.topology.Topology topology) {
+        List<Long> longs = prepareBucketsArray(buckets);
+
+        Map map = partitioner.algorithm().computeMapping(longs, rf, topology);
+    }
+
+    private static List<Long> prepareBucketsArray(int buckets) {
+        Long[] arr = new Long[buckets];
+        for (int i = 0; i < buckets; i++) {
+            arr[i] = (long) i;
+        }
+
+        return Arrays.asList(arr);
+    }
+}
