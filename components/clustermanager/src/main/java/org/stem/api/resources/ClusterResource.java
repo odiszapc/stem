8ea252bb6e8b25c7009fcc7f3428c586e2870fee
Alexey Plotnik <odiszapc@gmail.com>
2014-12-31 20:01:52 +1000

    STEM-6 #comment Accept: application/json to ClusterResource

--- components/clustermanager/src/main/java/org/stem/api/resources/ClusterResource.java
+++ components/clustermanager/src/main/java/org/stem/api/resources/ClusterResource.java
@@ -34,11 +34,12 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path(RESTConstants.Api.Cluster.URI)
-@Produces("application/json")
+@Produces(MediaType.APPLICATION_JSON)
 public class ClusterResource {
 
     /**
